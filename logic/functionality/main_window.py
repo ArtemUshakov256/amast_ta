657a61d5cebbe8d616fd880f0ddb7aebb75ebf09
Artem <ushakov.artem256@mail.ru>
2023-02-06 23:39:21 +0400

    ready convertation

--- logic/functionality/main_window.py
+++ logic/functionality/main_window.py
@@ -8,10 +8,11 @@ class MainWindow:
     def __init__(
         self,
         width=350,
-        height=200,
+        height=250,
         title="Основное окно",
         resizable=(False, False),
-        bg="#DFD8FF",
+        bg="#FFFFFF",
+        bg_pic=None,
         icon=None
     ):
         self.root = tk.Tk()
@@ -21,44 +22,67 @@ class MainWindow:
         self.root.config(bg=bg)
         if icon:
             self.root.iconbitmap(icon)
-        self.entry = tk.Entry(self.root,
+
+        self.canvas = tk.Canvas(self.root, height=height-100, width=width)
+        self.image_file = tk.PhotoImage(file=bg_pic)
+        self.image = self.canvas.create_image(0,0, anchor='nw', image=self.image_file)
+
+        self.entry = tk.Entry(
+            self.root,
             width=30,
             justify="left",
             relief="sunken",
-            bd=3)
+            bd=2
+        )
+        self.path_label = tk.Label(self.root, text="Путь к файлу", bg="#FFFFFF")
+        self.browse_button = tk.Button(self.root, text="Обзор", command=self.callback)
+        self.convert_button = tk.Button(
+            self.root, text="Конвертировать",
+            command=self.convert_txt_to_xlsx
+        )
+        # self.convertation_result = tk.Label(self.root, text=f"{self.result}", bg="#FFFFFF")
 
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
 
     def draw_widgets(self):
-        tk.Label(self.root, text="Путь к файлу", bg="#DFD8FF")\
-        .grid(row=0, column=0, stick="e")
-        
-        self.entry.grid(row=0, column=1)
+        self.canvas.pack(side='top')
 
-        tk.Button(self.root, text="Обзор", command=self.callback)\
-        .grid(row=0, column=2)
+        # self.path_label.grid(row=0, column=0, stick="e")
+        # self.entry.grid(row=0, column=1)
+        # self.browse_button.grid(row=0, column=2)
+        # self.convert_button.grid(row=1, column=1)
+
+        self.path_label.place(x=0, y=160)
+        self.entry.place(x=80, y=160)
+        self.browse_button.place(x=270, y=160)
+        self.convert_button.place(x=120, y=190)
+        # self.convertation_result.place(x=120, y=230)
 
     def callback(self):
-        file_path = make_path() 
-        self.entry.insert("insert", file_path)
+        self.file_path = make_path() 
+        self.entry.insert("insert", self.file_path)
 
-    def open_description_window(self,
-        width=400,
-        height=200,
-        title="Описание программы",
-        resizable=(False, False),
-        bg="#DFD8FF",
-        icon=None
-        ):
-        DescriptionWindow(self.root,
-            width,
-            height,
-            title,
-            resizable,
-            bg,
-            icon)
+    def convert_txt_to_xlsx(self):
+        self.result = extract_txt_data(path=self.file_path)
+        return self.result
+
+    # def open_description_window(self,
+    #     width=400,
+    #     height=200,
+    #     title="Описание программы",
+    #     resizable=(False, False),
+    #     bg="#DFD8FF",
+    #     icon=None
+    #     ):
+    #     DescriptionWindow(self.root,
+    #         width,
+    #         height,
+    #         title,
+    #         resizable,
+    #         bg,
+    #         icon)
 
 
 if __name__ == "__main__":
--- logic/utils.py
+++ logic/utils.py
@@ -1,6 +1,11 @@
+import pathlib
+import pandas as pd
 import re
 
+
+from pandas import ExcelWriter
 from tkinter import filedialog as fd
+
 
 def make_path():
     file_path = fd.askopenfilename(
@@ -10,23 +15,49 @@ def make_path():
     return file_path
 
 
-def extract_txt_data(path):
-    with open(path, "r") as file:
-        for line in file:
-            pass
-    with open(r"C:\Users\ushak\YandexDisk\Удаленка\Общий диск\Отчеты FTOWER автоматизация\типовая.txt", "r", encoding="ANSI") as file:
-        file_data = []
-        for line in file:
-            file_data.append(line.rstrip("\n"))
+def extract_txt_data(path=None):
+    if path:
+        with open(path, "r") as file:
+            for line in file:
+                pass
+        with open(path, "r", encoding="ANSI") as file:
+            file_data = []
+            for line in file:
+                file_data.append(line.rstrip("\n"))
 
-    # Foundation loads idexes extraction 
-    for i in range(len(file_data)):
-        file_data[i].rstrip("\n")
-        if re.match("Foundation loads", file_data[i]):
-            foundation_loads_start = i
-        if re.match("Summary of foundation loads", file_data[i]):
-            foundation_loads_end = i
-            break
-    foundation_loads_data = file_data[foundation_loads_start:foundation_loads_end]
+        # Foundation loads idexes extraction 
+        for i in range(len(file_data)):
+            file_data[i].rstrip("\n")
+            if re.match("Foundation loads", file_data[i]):
+                foundation_loads_start = i
+            if re.match("Summary of foundation loads", file_data[i]):
+                foundation_loads_end = i
+                break
+        foundation_loads_data = file_data[foundation_loads_start:foundation_loads_end]
 
-    # Something else..
+        # A part of "Case" and digit concatination 
+        for i in range(len(foundation_loads_data)):
+            if i not in [0, 2, len(foundation_loads_data)]:
+                foundation_loads_data[i] = foundation_loads_data[i].split()
+                if i == 1:
+                    temporary_list = []
+                    for j in range(len(foundation_loads_data[i])):
+                        if foundation_loads_data[i][j]=="Case":
+                            element = foundation_loads_data[i][j] + foundation_loads_data[i][j+1]
+                            temporary_list.append(element)
+                        elif foundation_loads_data[i][j] not in "123456789":
+                            element = foundation_loads_data[i][j]
+                            temporary_list.append(element)
+                    foundation_loads_data[i] = temporary_list         
+            else:
+                foundation_loads_data[i] = [foundation_loads_data[i]]
+
+        # Export data to xlsx
+        writer = ExcelWriter("Foundation_loads_data.xlsx")
+        df = pd.DataFrame(foundation_loads_data)
+        df.to_excel(writer, "Sheet1")
+        writer.close()
+
+        return "Файл конвертирован."
+    else:
+        return "Для конвертации сначала выберите файл!"
